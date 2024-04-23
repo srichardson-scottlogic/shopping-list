e15682abbb9fbeb96fb0866032c1cf9094632d8e
Sophie Richardson (She/Her) <srichardson@scottlogic.com>
2024-04-23 10:25:49 +0100

    Test safetest

--- shopping-list-web/src/main.tsx
+++ shopping-list-web/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { bootstrap } from "safetest/react";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById("root")!;
+const element = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+const isDev = process.env.NODE_ENV !== "production";
+
+bootstrap({
+  element,
+  render: (element) => ReactDOM.createRoot(container).render(element),
+
+  // Add one of the following depending on your bundler...
+
+  // Vite:
+  importGlob: isDev && import.meta.glob("./**/*.safetest.{j,t}s{,x}"),
+
+  // Other:
+  // import: isDev && async (s) => import(`${s.replace(/.*src/, '.').replace(/\.safetest$/, '')}.safetest`),
+});
